1dc112f88da20a8d96771d7f737ac767209c9d14
Luke Triantafyllidis <ltriant@cpan.org>
2023-04-30 15:38:49 +1000

    core: Shadow Mapping chapter progress
    Still got the aliasing problem to fix, but the screen dimensions
    doubling mid run caused a super frustrating problem to fix.

--- src/render3d.nim
+++ src/render3d.nim
@@ -1,44 +1,28 @@
 import glm
 import nimgl/[glfw, opengl]
-import stb_image/read as stbi
 
 import camera
 import model
 import shader
 
 import math
-import os
 
-# Screen dimensions
-const ScreenWidth  = 800
-const ScreenHeight = 600
+const
+  ScreenWidth  = 800
+  ScreenHeight = 600
+
+const
+  ShadowWidth = 1024
+  ShadowHeight = 1024
+
 
 # Camera
 var cam = createCamera()
-
-# Field of view
-var fov = 45.0f
 
 # Frame timing
 var deltaTime = 0.0f  # Time between current frame and last frame
 var lastFrame = 0.0f  # Time of last frame
 
-func intoNormalMatrix[A](m: Mat4x4[A]): Mat3x3[A] =
-  mat3(
-    vec3(m[0].arr[0], m[0].arr[1], m[0].arr[2]),
-    vec3(m[1].arr[0], m[1].arr[1], m[1].arr[2]),
-    vec3(m[2].arr[0], m[2].arr[1], m[2].arr[2])
-  )
-
-func intoMat4x4(m: Mat3x3[float32]): Mat4x4[float32] =
-  mat4(
-    vec4(m[0].arr[0], m[0].arr[1], m[0].arr[2], 0.0f),
-    vec4(m[1].arr[0], m[1].arr[1], m[1].arr[2], 0.0f),
-    vec4(m[2].arr[0], m[2].arr[1], m[2].arr[2], 0.0f),
-    vec4(0.0f, 0.0f, 0.0f, 0.0f),
-  )
-
-var blinn = false
 
 proc processInputs(window: GLFWWindow): void =
   if window.getKey(GLFWKey.Escape) == GLFWPress:
@@ -66,8 +50,6 @@ proc processInputs(window: GLFWWindow): void =
   if window.getKey(GLFWKey.D) == GLFW_Press:
     cam.processInput CameraMovement.camRight, deltaTime
 
-  blinn = window.getKey(GLFWKey.B) == GLFW_Press
-
   glPolygonMode(
     GL_FRONT_AND_BACK,
     if window.getKey(GLFWKey.Space) == GLFW_Press:
@@ -76,6 +58,10 @@ proc processInputs(window: GLFWWindow): void =
       GL_FILL
   )
 
+
+# Field of view
+var fov = 45.0f
+
 proc scrollCallback(window: GLFWWindow, xoffset: float64, yoffset: float64): void {.cdecl.} =
   fov -= yoffset.float
 
@@ -83,6 +69,7 @@ proc scrollCallback(window: GLFWWindow, xoffset: float64, yoffset: float64): voi
     fov = 1.0f
   elif fov > 45.0f:
     fov = 45.0f
+
 
 var firstMouse = true
 var lastX = 400.0f
@@ -109,6 +96,107 @@ proc mouseCallback(window: GLFWWindow, xpos: float64, ypos: float64): void {.cde
     return
 
   cam.processMouseInput xoffset, yoffset
+
+
+var
+  cubeVAO: GLuint = 0
+  cubeVBO: GLuint = 0
+
+proc renderCube() =
+  if cubeVAO == 0:
+    var cubeVertices = [
+      # back face
+      -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, # bottom-left
+       1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, # top-right
+       1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, # bottom-right
+       1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, # top-right
+      -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, # bottom-left
+      -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, # top-left
+      # front face
+      -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, # bottom-left
+       1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, # bottom-right
+       1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, # top-right
+       1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, # top-right
+      -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, # top-left
+      -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, # bottom-left
+      # left face
+      -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, # top-right
+      -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, # top-left
+      -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, # bottom-left
+      -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, # bottom-left
+      -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, # bottom-right
+      -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, # top-right
+      # right face
+       1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, # top-left
+       1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, # bottom-right
+       1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, # top-right
+       1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, # bottom-right
+       1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, # top-left
+       1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, # bottom-left
+      # bottom face
+      -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, # top-right
+       1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, # top-left
+       1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, # bottom-left
+       1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, # bottom-left
+      -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, # bottom-right
+      -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, # top-right
+      # top face
+      -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, # top-left
+       1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, # bottom-right
+       1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, # top-right
+       1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, # bottom-right
+      -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, # top-left
+      -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  # bottom-left
+    ]
+
+    glGenVertexArrays 1, cubeVAO.addr
+    glGenBuffers 1, cubeVBO.addr
+    glBindVertexArray cubeVAO
+    glBindBuffer GL_ARRAY_BUFFER, cubeVBO
+    glBufferData GL_ARRAY_BUFFER, sizeof(cfloat) * len(cubeVertices), cubeVertices[0].addr, GL_STATIC_DRAW
+    glEnableVertexAttribArray 0
+    glVertexAttribPointer 0, 3, EGL_FLOAT, false, 8 * sizeof(cfloat), cast[pointer](0)
+    glEnableVertexAttribArray 1
+    glVertexAttribPointer 1, 3, EGL_FLOAT, false, 8 * sizeof(cfloat), cast[pointer](3 * sizeof(cfloat))
+    glEnableVertexAttribArray 2
+    glVertexAttribPointer 2, 2, EGL_FLOAT, false, 8 * sizeof(cfloat), cast[pointer](6 * sizeof(cfloat))
+    glBindBuffer GL_ARRAY_BUFFER, 0
+    glBindVertexArray 0
+
+  glBindVertexArray cubeVAO
+  glDrawArrays GL_TRIANGLES, 0, 36
+  glBindVertexArray 0
+
+
+var planeVAO: GLuint
+
+proc renderScene(shader: Shader) =
+  # Floor
+  var model = mat4f(1.0)
+    .translate(vec3f(0.0, -0.1, 0.0))
+  shader.setMat4x4f "model", model
+  glBindVertexArray planeVAO
+  glDrawArrays GL_TRIANGLES, 0, 6
+
+  # Cubes
+  model = mat4f(1.0)
+    .translate(vec3f(0.0, 1.5, 0.0))
+    .scale(vec3f(0.5))
+  shader.setMat4x4f "model", model
+  renderCube()
+
+  model = mat4f(1.0)
+    .translate(vec3f(2.0, 0.0, 1.0))
+    .scale(vec3f(0.5))
+  shader.setMat4x4f "model", model
+  renderCube()
+
+  model = mat4f(1.0)
+    .translate(vec3f(-1.0, 0.0, 2.0))
+    .rotate(radians(60.0f), normalize(vec3f(1.0, 0.0, 1.0)))
+    .scale(vec3f(0.25))
+  shader.setMat4x4f "model", model
+  renderCube()
 
 
 when isMainModule:
@@ -138,40 +226,85 @@ when isMainModule:
   glEnable GL_DEPTH_TEST
 
   var
-    #myModel = loadModel("obj/backpack/backpack.obj")
+    # Load shaders
     myShader = newShader("shader-advanced.vs", "shader-advanced.fs")
+    simpleDepthShader = newShader("shader-shadowMapping.vs", "shader-shadowMapping.fs")
+    debugDepthQuad = newShader("shader-debugQuad.vs", "shader-debugQuad.fs")
 
+    # Load textures
+    woodTexture = loadTexture("wood.png")
+
+
+  #
+  # Setup the plane/floor
+  #
   var
-    planeVAO: GLuint
+    #planeVAO: GLuint
     planeVBO: GLuint
     planeVertices = [
-      10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
-      -10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
-      -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
+      # positions             # normals          # texcoords
+       25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+      -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+      -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
 
-      10.0f, -0.5f,  10.0f,  0.0f, 1.0f, 0.0f,  10.0f,  0.0f,
-      -10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 10.0f,
-      10.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,  10.0f, 10.0f
+       25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+      -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+       25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
     ]
 
-  glGenVertexArrays(1, planeVAO.addr);
-  glGenBuffers(1, planeVBO.addr);
-  glBindVertexArray(planeVAO);
-  glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
-  glBufferData(GL_ARRAY_BUFFER, cfloat.sizeof * planeVertices.len, planeVertices[0].addr, GL_STATIC_DRAW);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, EGL_FLOAT, false, 8 * sizeof(cfloat), cast[pointer](0));
-  glEnableVertexAttribArray(1);
-  glVertexAttribPointer(1, 3, EGL_FLOAT, false, 8 * sizeof(cfloat), cast[pointer](3 * sizeof(cfloat)));
-  glEnableVertexAttribArray(2);
-  glVertexAttribPointer(2, 2, EGL_FLOAT, false, 8 * sizeof(cfloat), cast[pointer](6 * sizeof(cfloat)));
-  glBindVertexArray(0);
-
-  var floorTexture = loadTexture("wood.png")
-  myShader.setInt("texture1", 0)
+  glGenVertexArrays 1, planeVAO.addr
+  glGenBuffers 1, planeVBO.addr
+  glBindVertexArray planeVAO
+  glBindBuffer GL_ARRAY_BUFFER, planeVBO
+  glBufferData GL_ARRAY_BUFFER, sizeof(cfloat) * planeVertices.len, planeVertices[0].addr, GL_STATIC_DRAW
+  glEnableVertexAttribArray 0
+  glVertexAttribPointer 0, 3, EGL_FLOAT, false, 8 * sizeof(cfloat), cast[pointer](0)
+  glEnableVertexAttribArray 1
+  glVertexAttribPointer 1, 3, EGL_FLOAT, false, 8 * sizeof(cfloat), cast[pointer](3 * sizeof(cfloat))
+  glEnableVertexAttribArray 2
+  glVertexAttribPointer 2, 2, EGL_FLOAT, false, 8 * sizeof(cfloat), cast[pointer](6 * sizeof(cfloat))
+  glBindVertexArray 0
 
 
-  var lightPos = vec3f(0.0, 0.0, 0.0)
+  #
+  # Depth map FBO
+  #
+  var
+    depthMapFBO: GLuint
+    depthMap: GLuint
+
+  glGenFramebuffers 1, depthMapFBO.addr
+  glGenTextures 1, depthMap.addr
+  glBindTexture GL_TEXTURE_2D, depthMap
+  glTexImage2D GL_TEXTURE_2D, 0, GLint(GL_DEPTH_COMPONENT), ShadowWidth, ShadowHeight, 0, GL_DEPTH_COMPONENT, EGL_FLOAT, nil
+  glTexParameteri GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GLint(GL_NEAREST)
+  glTexParameteri GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GLint(GL_NEAREST)
+  glTexParameteri GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GLint(GL_REPEAT)
+  glTexParameteri GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GLint(GL_REPEAT)
+
+  glBindFramebuffer GL_FRAMEBUFFER, depthMapFBO
+  glFramebufferTexture2D GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0
+  glDrawBuffer GL_NONE
+  glReadBuffer GL_NONE
+  glBindFramebuffer GL_FRAMEBUFFER, 0
+
+
+  #
+  # Shader configuration
+  #
+  myShader.use
+  myShader.setInt "diffuseTexture", 0
+  myShader.setInt "shadowMap", 1
+
+  debugDepthQuad.use
+  debugDepthQuad.setInt "depthMap", 0
+
+
+  #
+  # Light info
+  #
+  var lightPos = vec3f(-2.0, 4.0, -1.0)
+
   while not w.windowShouldClose:
     # Frame timing
     let currentFrame = glfwGetTime()
@@ -182,46 +315,94 @@ when isMainModule:
     processInputs w
 
     # Render
-    glEnable GL_DEPTH_TEST
-
-    glClearColor 0.20f, 0.28f, 0.35f, 1.0f
+    glClearColor 0.1f, 0.1f, 0.1f, 1.0f
     glClear GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT
 
-    # Transformation matrices
+    let
+      nearPlane = 1.0f
+      farPlane = 7.5f
+
+    var
+      lightProjection = ortho(
+        -10.0f,
+        10.0f,
+        -10.0f,
+        10.0f,
+        nearPlane,
+        farPlane
+      )
+      lightView = lookAt(
+        lightPos,
+        vec3f(0.0),
+        vec3f(0.0, 1.0, 0.0)
+      )
+      lightSpaceMatrix = lightProjection * lightView
+
+    simpleDepthShader.use
+    simpleDepthShader.setMat4x4f "lightSpaceMatrix", lightSpaceMatrix
+
+    #
+    # 1. Render the depth map
+    #
+    glViewport 0, 0, ShadowWidth, ShadowHeight
+    glBindFramebuffer GL_FRAMEBUFFER, depthMapFBO
+    glClear GL_DEPTH_BUFFER_BIT
+    glActiveTexture GL_TEXTURE0
+    glBindTexture GL_TEXTURE_2D, woodTexture
+    renderScene simpleDepthShader
+    glBindFramebuffer GL_FRAMEBUFFER, 0
+
+    # Reset the viewport
+    # I don't know why I need to do it this way, but for some reason at this point the screen
+    # width has changed from 800 to 1600, and the screen heigh has changed from 600 to 1200. So if
+    # I try to use the original constants, everything is only rendered to the bottom left quadrant
+    # of the window.
+    var
+      scrWidth: int32
+      scrHeight: int32
+    w.getFramebufferSize scrWidth.addr, scrHeight.addr
+    glViewport 0, 0, scrWidth, scrHeight
+
+    glClear GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT
+
+    #
+    # 2. Render the scene
+    #
     var projection = perspective(
       fov.radians,
-      ScreenWidth.float / ScreenHeight.float,
+      scrWidth.float / scrHeight.float,
       0.1,
       100.0
     )
     var view = cam.lookAtMatrix()
-    var model = mat4f(1.0)
-
     myShader.use
-    
     myShader.setMat4x4f "projection", projection
     myShader.setMat4x4f "view", view
-    myShader.setMat4x4f "model", model
 
-    #myShader.setVec3f "material.ambient", vec3f(1.0, 0.5, 0.31)
-    #myShader.setVec3f "material.diffuse", vec3f(1.0, 0.5, 0.31)
-    #myShader.setVec3f "material.specular", vec3f(0.5, 0.5, 0.5)
-    #myShader.setFloat "material.shininess", 32.0f
-    myShader.setVec3f "lightPos", lightPos
+    # Set light uniforms
     myShader.setVec3f "viewPos", cam.position
-    myShader.setInt "blinn", blinn.int
+    myShader.setVec3f "lightPos", lightPos
+    myShader.setMat4x4f "lightSpaceMatrix", lightSpaceMatrix
 
-    glBindVertexArray(planeVAO);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, floorTexture);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
-    #myModel.draw myShader
+    # Bind textures and render
+    glActiveTexture GL_TEXTURE0
+    glBindTexture GL_TEXTURE_2D, woodTexture
+    glActiveTexture GL_TEXTURE1
+    glBindTexture GL_TEXTURE_2D, depthMap
+    renderScene myShader
+
+    #
+    # 3. Render debugging quad
+    #
+    #[debugDepthQuad.use
+    debugDepthQuad.setFloat "near_plane", nearPlane
+    debugDepthQuad.setFloat "far_plane", farPlane
+    glActiveTexture GL_TEXTURE0;
+    glBindTexture GL_TEXTURE_2D, depthMap
+    #renderQuad debugDepthQuad]#
 
     w.swapBuffers
     glfwPollEvents()
-
-  # Clean up
-  #delete myModel
 
   w.destroyWindow
   glfwTerminate()
